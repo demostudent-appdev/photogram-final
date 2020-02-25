c4c324ee546bde088e27bc91b9728648521f7bb4
Carlos Trejo <carlos.trejo@chicagobooth.edu>
2020-02-25 01:00:26 +0000

    DB Model creation with dummy data

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  comments_count  :integer
+#  email           :string
+#  is_private      :boolean
+#  likes_count     :integer
+#  password_digest :string
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  
+  validates(:username,
+    {
+      :presence => true,
+      :uniqueness => { :case_sensitive => false }
+    }
+  )
+
+  has_secure_password
+end
