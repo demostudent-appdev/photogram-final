a85aa602a638a8de62541eb7f97fde3266eafc61
Carlos Trejo <carlos.trejo@chicagobooth.edu>
2020-02-25 01:20:08 +0000

    DB Model creation with draft:resource + dummy data

--- README.md
+++ README.md
@@ -39,3 +39,5 @@ Look around [the target](http://photogram-final.matchthetarget.com/) and try to 
   - it lists the photos that are liked by the people the user is following
 
 
+rails g draft:resource photo owner_id:integer caption:text likes_count:integer comments_count:integer image:string
+rails g draft:resource photo caption:text image:string owner_id:integer comments_count:integer likes_count:integer
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,42 @@
 Rails.application.routes.draw do
+  # Routes for the User resource:
+
+  # CREATE
+  post("/insert_user", { :controller => "users", :action => "create" })
+          
+  # READ
+  get("/users", { :controller => "users", :action => "index" })
+  
+  get("/users/:path_id", { :controller => "users", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_user/:path_id", { :controller => "users", :action => "update" })
+  
+  # DELETE
+  get("/delete_user/:path_id", { :controller => "users", :action => "destroy" })
+
+  #------------------------------
+
+  # Routes for the Photo resource:
+
+  # CREATE
+  post("/insert_photo", { :controller => "photos", :action => "create" })
+          
+  # READ
+  get("/photos", { :controller => "photos", :action => "index" })
+  
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_photo/:path_id", { :controller => "photos", :action => "update" })
+  
+  # DELETE
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
+
+  #------------------------------
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
