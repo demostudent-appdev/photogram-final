12c960f7e7149c7e1c70729bdb2bf7bf3c1c59c1
Carlos Trejo <carlos.trejo@chicagobooth.edu>
2020-02-29 19:19:46 +0000

    DB creation with model, resource & account drafts

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,59 @@
+class UsersController < ApplicationController
+  # skip_before_action(:force_user_sign_in, { :only => [:new_registration_form, :create] })
+  
+  def new_registration_form
+    render({ :template => "user_sessions/sign_up.html.erb" })
+  end
+
+  def create
+    @user = User.new
+    @user.is_private = params.fetch("query_is_private", false)
+    @user.username = params.fetch("query_username")
+    @user.email = params.fetch("query_email")
+    @user.password = params.fetch("query_password")
+    @user.password_confirmation = params.fetch("query_password_confirmation")
+    @user.comments_count = params.fetch("query_comments_count")
+    @user.likes_count = params.fetch("query_likes_count")
+
+    save_status = @user.save
+
+    if save_status == true
+      session.store(:user_id,  @user.id)
+   
+      redirect_to("/", { :notice => "User account created successfully."})
+    else
+      redirect_to("/user_sign_up", { :alert => "User account failed to create successfully."})
+    end
+  end
+    
+  def edit_registration_form
+    render({ :template => "users/edit_profile.html.erb" })
+  end
+
+  def update
+    @user = @current_user
+    @user.is_private = params.fetch("query_is_private", false)
+    @user.username = params.fetch("query_username")
+    @user.email = params.fetch("query_email")
+    @user.password = params.fetch("query_password")
+    @user.password_confirmation = params.fetch("query_password_confirmation")
+    @user.comments_count = params.fetch("query_comments_count")
+    @user.likes_count = params.fetch("query_likes_count")
+    
+    if @user.valid?
+      @user.save
+
+      redirect_to("/", { :notice => "User account updated successfully."})
+    else
+      render({ :template => "users/edit_profile_with_errors.html.erb" })
+    end
+  end
+
+  def destroy
+    @current_user.destroy
+    reset_session
+    
+    redirect_to("/", { :notice => "User account cancelled" })
+  end
+  
+end
